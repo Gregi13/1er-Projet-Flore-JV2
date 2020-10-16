d3066a4eddfe0fd4db581bfafa41c1a18b9cf8d9
Victor <thewolfy67@gmail.com>
2020-10-16 16:18:26 +0200

    ajout des RulesTiles

--- Assets/Script/PlayerBehaviour.cs
+++ Assets/Script/PlayerBehaviour.cs
@@ -1,9 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerBehaviour : MonoBehaviour
 {
+    private Inputs inputs;
+
+    private void OnEnable()
+
+    {
+        inputs = new Inputs();
+        inputs.Enable();
+        inputs.Player.Move.performed += OnMovePerformed;
+    }
+
+    private void OnMovePerformed(InputAction.CallbackContext obj)
+    {
+        Debug.Log("Move fonctionne");
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
