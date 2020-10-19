fc36089ab720fe20d845cd06af008a6f605a6551
Victor <thewolfy67@gmail.com>
2020-10-19 16:36:15 +0200

    Déplacements totaux finis (flip)
    Et refaire les timeslaps

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -8,9 +8,14 @@ public class PlayerBehaviour : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private float maxSpeed;
+    
     private Inputs inputs;
     private Vector2 direction;
 
+    private Rigidbody2D myRigidbody;
+    private Animator myAnimator;
+    private SpriteRenderer myRenderer;
+    
     private void OnEnable()
 
     {
@@ -18,31 +23,41 @@ public class PlayerBehaviour : MonoBehaviour
         inputs.Enable();
         inputs.Player.Move.performed += OnMovePerformed;
         inputs.Player.Move.canceled += OnMoveCanceled;
-    }
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myRenderer = GetComponent<SpriteRenderer>();
 
+    }
     private void OnMoveCanceled(InputAction.CallbackContext obj) 
     {
         direction = Vector2.zero;
     }
-    
-    
     private void OnMovePerformed(InputAction.CallbackContext obj)
     {   
         direction = obj.ReadValue<Vector2>();
     }
-
-
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        //GetComponent<Animator>().SetBool("IsRunning", true);
     }
 
     void FixedUpdate()
     {
-        var myRigidBody = GetComponent<Rigidbody2D>();
         direction.y = 0;
-        if (myRigidBody.velocity.sqrMagnitude < maxSpeed) 
-            myRigidBody.AddForce(direction * speed);
+        if (myRigidbody.velocity.sqrMagnitude < maxSpeed) 
+            myRigidbody.AddForce(direction * speed);
+        var IsRunning = direction.x != 0;
+        myAnimator.SetBool("IsRunning", IsRunning);
+        if (direction.x < 0)
+        {
+            myRenderer.flipX = true;
+        }
+        
+        else if (direction.x > 0)
+        {
+            myRenderer.flipX = false;
+        }
     }
 }
