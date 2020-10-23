f54218ef1e5f5ad90cd7ca3c113e98326361f0f8
Victor <thewolfy67@gmail.com>
2020-10-23 16:53:13 +0200

    FINAL

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void Jouer()
+    {
+        SceneManager.LoadScene("SampleScene"); //permet de charger la scene "SampleScene"
+    }
+    
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits");    //permet de charger la scene "Credits"
+    }
+    
+    public void Quitter()
+    {
+        Application.Quit();                    //permet de quitter l'application lorsque l'on appuie sur le bouton
+        Debug.Log("Ferme le jeu");
+    }
+    
+    public void Leave()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+}
